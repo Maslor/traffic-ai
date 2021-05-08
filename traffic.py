3c366b395ff512a33dffd771ac39813c2221a971
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2021-05-09 00:15:56 +0100

    Better performance, around 80% accuracy

--- traffic.py
+++ traffic.py
@@ -78,22 +78,11 @@ def get_model():
     """
     model = tf.keras.models.Sequential([
 
-        # Convolutional layer. Learn 32 filters using a 3x3 kernel
-        tf.keras.layers.Conv2D(
-            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
-        ),
-
-        # Max-pooling layer, using 2x2 pool size
-        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
-
         # Flatten units
-        tf.keras.layers.Flatten(),
-
-        # Add a hidden layer with dropout
-        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Flatten(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
 
         # Add an output layer with output units for all 10 digits
-        tf.keras.layers.Dense(NUM_CATEGORIES, activation="relu")
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
 
     model.compile(
