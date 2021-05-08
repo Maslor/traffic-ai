2080b8b78039f96ca0f88e4794f2c15bc500a5ff
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2021-05-09 00:12:18 +0100

    first implementation, low accuracy of around 10-15%

--- traffic.py
+++ traffic.py
@@ -14,7 +14,6 @@ TEST_SIZE = 0.4
 
 
 def main():
-
     # Check command-line arguments
     if len(sys.argv) not in [2, 3]:
         sys.exit("Usage: python traffic.py data_directory [model.h5]")
@@ -35,7 +34,7 @@ def main():
     model.fit(x_train, y_train, epochs=EPOCHS)
 
     # Evaluate neural network performance
-    model.evaluate(x_test,  y_test, verbose=2)
+    model.evaluate(x_test, y_test, verbose=2)
 
     # Save model to file
     if len(sys.argv) == 3:
@@ -58,7 +57,17 @@ def load_data(data_dir):
     be a list of integer labels, representing the categories for each of the
     corresponding `images`.
     """
-    raise NotImplementedError
+    directories = sorted([name for name in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, name))])
+    images = []
+    labels = []
+
+    for directory in directories:
+        for file in os.listdir(os.path.join(data_dir, directory)):
+            image = cv2.resize(cv2.imread(os.path.join(data_dir, directory, file)), (IMG_WIDTH, IMG_HEIGHT))
+            images.append(image)
+            labels.append(int(directory))
+
+    return images, labels
 
 
 def get_model():
@@ -67,7 +76,33 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    raise NotImplementedError
+    model = tf.keras.models.Sequential([
+
+        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        tf.keras.layers.Conv2D(
+            64, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+        ),
+
+        # Max-pooling layer, using 2x2 pool size
+        tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+
+        # Flatten units
+        tf.keras.layers.Flatten(),
+
+        # Add a hidden layer with dropout
+        tf.keras.layers.Dense(64, activation="relu"),
+
+        # Add an output layer with output units for all 10 digits
+        tf.keras.layers.Dense(NUM_CATEGORIES, activation="relu")
+    ])
+
+    model.compile(
+        optimizer="adam",
+        loss="categorical_crossentropy",
+        metrics=["accuracy"]
+    )
+
+    return model
 
 
 if __name__ == "__main__":
