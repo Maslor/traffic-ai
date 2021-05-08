9e26c523b091ec34a7e72363339752d2e3c1b915
Gabriel Freire <gabriel.freire@tecnico.ulisboa.pt>
2021-05-09 00:26:31 +0100

    Better performance, around 80% accuracy

--- traffic.py
+++ traffic.py
@@ -81,7 +81,7 @@ def get_model():
         # Flatten units
         tf.keras.layers.Flatten(input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)),
 
-        # Add an output layer with output units for all 10 digits
+        # Output Layer
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
 
